d76dde8cc0a44c17ada89c6691b95fce0659219f
Rudy Alvarez <rudygt@gmail.com>
2016-08-03 21:56:28 -0600

    added nancy adapter project

--- Src/Adapters/Nancy.Metrics/Properties/AssemblyInfo.cs
+++ Src/Adapters/Nancy.Metrics/Properties/AssemblyInfo.cs
@@ -1,4 +1,19 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Nancy.Metrics")]
-[assembly: AssemblyDescription("NancyFx adapter for the Metrics.NET Library")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nancy.Metrics")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36cac446-173e-40f7-a811-5cece4c15a6b")]
